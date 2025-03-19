76adc76a9209ceb168ab0bc881c10d7c199f7df1
tcheiner <tcheiner@gmail.com>
2025-03-20 00:06:36 +0100

    removed starlight.  set node to 22.13.10

--- astro.config.ts
+++ astro.config.ts
@@ -2,7 +2,6 @@ import {defineConfig} from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
